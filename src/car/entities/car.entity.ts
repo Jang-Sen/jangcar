428538b476ac19e36243b66be8ab84644bc019c9
jangsen <dh7895210@naver.com>
2024-10-09 21:44:08 +0900

    car 엔티티 추가

--- src/car/entities/car.entity.ts
+++ src/car/entities/car.entity.ts
@@ -1,1 +1,17 @@
-export class Car {}
+import { Column, Entity } from 'typeorm';
+import { Base } from './base.entity';
+
+@Entity()
+export class Car extends Base {
+  @Column()
+  public carName: string;
+
+  @Column()
+  public price: number;
+
+  @Column()
+  public category: string;
+
+  @Column()
+  public carImg: string;
+}
