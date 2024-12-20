2907425c8120bacb99c6671d582e18e457474ce3
jangsen <dh7895210@naver.com>
2024-12-20 15:23:26 +0900

    One To Many #3 수정 & 삭제(본인만)

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -1,9 +1,11 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   Param,
   Post,
+  Put,
   Req,
   UseGuards,
 } from '@nestjs/common';
@@ -12,6 +14,7 @@ import { ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
 import { RequestUserInterface } from '@auth/interface/requestUser.interface';
 import { CreateCommentDto } from '@comment/dto/create-comment.dto';
 import { AccessTokenGuard } from '@auth/guard/accessToken.guard';
+import { UpdateCommentDto } from '@comment/dto/update-comment.dto';
 
 @ApiTags('Comment')
 @Controller('comment')
@@ -47,5 +50,44 @@ export class CommentController {
     @Body() dto: CreateCommentDto,
   ) {
     return await this.commentService.createComment(req.user, carId, dto);
+  }
+
+  @Put('/:commentId')
+  @UseGuards(AccessTokenGuard)
+  @ApiParam({
+    name: 'commentId',
+    description: 'Comment ID',
+  })
+  @ApiOperation({
+    summary: '댓글 수정',
+    description: '작성자 본인만 수정 가능',
+  })
+  async updateCommentOnlySelf(
+    @Req() req: RequestUserInterface,
+    @Param('commentId') commentId: string,
+    @Body() dto: UpdateCommentDto,
+  ) {
+    return await this.commentService.updateCommentOnlySelf(
+      req.user,
+      commentId,
+      dto,
+    );
+  }
+
+  @Delete('/:commentId')
+  @UseGuards(AccessTokenGuard)
+  @ApiParam({
+    name: 'commentId',
+    description: 'Comment ID',
+  })
+  @ApiOperation({
+    summary: '댓글 삭제',
+    description: '작성자 본인만 삭제 가능',
+  })
+  async deleteCommentOnlySelf(
+    @Req() req: RequestUserInterface,
+    @Param('commentId') commentId: string,
+  ) {
+    return await this.commentService.deleteCommentOnlySelf(req.user, commentId);
   }
 }
--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,10 +1,15 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Comment } from '@comment/entities/comment.entity';
 import { Repository } from 'typeorm';
 import { CarService } from '@car/car.service';
 import { User } from '@user/entities/user.entity';
 import { CreateCommentDto } from '@comment/dto/create-comment.dto';
+import { UpdateCommentDto } from '@comment/dto/update-comment.dto';
 
 @Injectable()
 export class CommentService {
@@ -50,5 +55,58 @@ export class CommentService {
     await this.repository.save(comment);
 
     return comment;
+  }
+
+  // 수정
+  async updateCommentOnlySelf(
+    user: User,
+    commentId: string,
+    commentDto: UpdateCommentDto,
+  ): Promise<string> {
+    const comment = await this.repository.findOne({
+      where: {
+        id: commentId,
+      },
+      relations: {
+        user: true,
+      },
+    });
+
+    if (!comment) {
+      throw new NotFoundException('댓글을 찾을 수 없습니다.');
+    }
+
+    if (comment.user.id !== user.id) {
+      throw new ForbiddenException('본인이 작성한 댓글만 수정 가능합니다.');
+    }
+
+    Object.assign(comment, commentDto);
+    await this.repository.save(comment);
+
+    return '수정 완료';
+  }
+
+  // 삭제
+  async deleteCommentOnlySelf(user: User, commentId: string): Promise<string> {
+    const comment = await this.repository.findOne({
+      where: {
+        id: commentId,
+      },
+      relations: {
+        user: true,
+      },
+    });
+
+    if (!comment) {
+      throw new NotFoundException('댓글을 찾을 수 없습니다.');
+    }
+
+    if (comment.user.id !== user.id) {
+      throw new ForbiddenException('본인이 작성한 댓글만 수정 가능합니다.');
+    }
+
+    await this.repository.remove(comment);
+
+    return '삭제 완료';
   }
 }
